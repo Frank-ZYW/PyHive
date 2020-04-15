9e64ab564b3cf4b93d3cdcc85afaa865d6cae80c
Frank ZYW <Frank_zhu_@outlook.com>
2020-04-15 14:35:36 +0800

    fix volume settings

--- docker/run.sh
+++ docker/run.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+dir="/app/pyhive"
+
+is_empty_dir(){
+    return `ls -A $1|wc -w`
+}
+
+if is_empty_dir $(dir)
+then
+    cd /app
+    pyhive init
+    cd pyhive
+    pyhive migrate
+    pyhive initadmin
+    pyhive collectstatic
+fi
+
 cd /app/pyhive
 uwsgi --ini uwsgi.ini
 service nginx start
